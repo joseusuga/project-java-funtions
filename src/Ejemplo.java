10f245052f6ab51c583b0d6f970e3f5f91351106
jose usuga <jose1028.m@gmail.com>
2025-05-13 23:08:37 -0500

    función sin parámetros y con retorno implementada

---
+++ src/Ejemplo.java
@@ -0,0 +1,11 @@
+public class Ejemplo {
+
+    public static String obtenerMensaje() {
+        return "¡Hola, jente de adso!";
+    }
+
+    public static void main(String[] args) {
+        String mensaje = obtenerMensaje();
+        System.out.println(mensaje);
+    }
+}
