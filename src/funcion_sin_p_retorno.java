54807552ecfa881da76c1f730ab08d1802a98511
jose usuga <jose1028.m@gmail.com>
2025-05-13 22:37:40 -0500

    función sin parámetros y sin retorno implementada

---
+++ src/funcion_sin_p_retorno.java
@@ -0,0 +1,12 @@
+public class funcion_sin_p_retorno {
+
+    public static void mostrarBienvenida() {
+        System.out.println("¡Bienvenido al sistema de gestión de usuarios!");
+        System.out.println("Por favor, seleccione una opción del menú.");
+        System.out.println("gracias, buelva pronto");
+    }
+
+    public static void main(String[] args) {
+        mostrarBienvenida();
+    }
+}
